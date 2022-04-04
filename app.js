18b5d9f3c778d0adf79876d0097ab8d2339043da
muhammad naveed <amirprinceak5@gmail.com>
2022-04-04 09:09:45 +0500

    complete

---
+++ app.js
@@ -0,0 +1,164 @@
+                             //CHAPTER NO1 
+
+ //  Question no: 1
+
+// var string=("error! lease enter a valiid password")
+// alert( string );
+                            
+
+// Question no: 2
+
+ //var stringOne=("welcome to JS land... </br>   Happy Coding! ")
+ //alert( stringOne );
+
+// Question no: 3
+
+ //var stringOne=("welcome to JS Land...");
+ //alert(stringOne);
+ //var stringTwo=("happy coding!  prevent this page from creating aditional dialogs ");
+ //alert(stringTwo);
+ 
+ 
+ // Question no: 5
+ //var stringone=("hello... i can run js through my web browser's console");
+ //alert( stringone);
+  
+                        //////////////////END//////////////////////////
+
+                                   //CHAPTER NO2
+/// Question no: 1
+
+// var userName=("MUHAMMAD NAVEED");
+// alert(userName);
+// var age=(22);
+// alert(age)
+// var message=("certified mobile application development");
+// alert(message)
+
+// Question no: 2
+
+//var food=("pizza");
+// alert(food);
+
+
+// Question no: 3
+
+ //var email=("My Email address is amirprinceak5@gmail.com");
+//alert(email);
+
+// Question no: 4
+
+// var book=("i am trying to learn from the book A samrter way to learn javaScript");
+// alert(book);
+
+// Question no: 5 
+
+//  document.write("Yah! i can write HTML content through js ");
+
+//Question no: 6 pandding
+ 
+//var  string=(---------@@@----------);
+ // alert(string);
+                 
+                    //////////////////END//////////////////////////
+
+                                   // Chapter no: 03
+ // Question no: 1 
+  
+  //var age=( "i am  15 year old");
+  //alert(age);
+
+// Question no: 2 
+  
+//var string=("you have visited this site 14 times");
+// alert(string);
+
+// Question no: 3 
+
+ //var birth=("<b>my birth year is 1990</b>");
+ //document.write(birth);
+
+// Question no: 4 
+
+ //var Name=("john orderd");
+ //document.write(Name);
+ //var qty=( 21 );
+ //document.write(qty);
+ //var product=("T-shirt from J. Company");
+ //document.write(product);
+
+                         //////////////////END//////////////////////////
+
+                                // Chapter no: 04 
+// Question no: 1 
+
+ //var heading=("<h1><b>Rules for naming JS variables</b></h1>");
+ //document.write(heading);
+ //var values=("variable name can only contaiN NUMBER $ AND FOR EXAMPLE my 1st variable");
+ //document.write(values);
+ //var sentence=("</br> varaible must begin with a letter or for example  name name or name");
+ //document.write(sentence);
+ //var sentence1=("</br> variable name are case sensetive ");
+ //document.write(sentence1);
+ //var string=("</br> variable name shuld not be   JS keywords ");
+ //document.write(string);
+                        
+                    
+                          //////////////////END//////////////////////////
+
+                          
+                                  // Chapter no: 05 
+// Question no: 1 
+
+ //var num1=(3);
+ //var num2=(5);
+ //var result=(3+5)
+  //document.write("sum of 3 and 5 is", + result);
+
+// Question no: 2 
+
+//var num1=(2);
+ //var num2=(4);
+ //var add=(2+4);
+ //document.write(" value after addition is : ", add)
+ //var sub=(4-2);
+ //document.write("<br/>value after subtraction is : ",sub);
+ //var product=(2*4);
+ //document.write("<br/>value after multiplication is : ",product);
+ //var div=(4/2);
+ //document.write("<br/>value after division is : ",div);
+ //var modulus=(4%2);
+ //document.write("<br/>value of remainder is : ",modulus);
+
+/// Question no: 3 
+
+ //var num1=(600);
+ //var product=(600*5);
+ //document.write("total cost to buy 5 ticket to a Movie is : PKR",product);
+
+/// Question no: 4 
+
+ //var num1=(6);
+ //var num2=(1);
+ //var num3=(2);
+ //var num4=(3);
+ //var num5=(4);
+ //var num6=(5);
+ //var num7=(6);
+ //var num8=(7);
+ //var num9=(8);
+ //var num10=(9);
+ //var num11=(10);
+ //document.write("<h2>Table 6 </h2>")
+ //document.write("6 x 1 = ",num1*num2);
+ //document.write("<br /> 6 x 2 = ",num1*num3);
+ //document.write("<br /> 6 x 3 = ",num1*num4);
+ //document.write("<br /> 6 x 4 = ",num1*num5);
+ //document.write("<br /> 6 x 5 = ",num1*num6);
+ //document.write("<br /> 6 x 6 = ",num1*num7);
+ //document.write("<br /> 6 x 7 = ",num1*num8);
+ //document.write("<br /> 6 x 8 = ",num1*num9);
+ //document.write("<br /> 6 x 9 = ",num1*num10);
+ //document.write("<br /> 6 x 10 = ",num1*num11);
+
+
